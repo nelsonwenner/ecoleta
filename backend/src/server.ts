5033ce7f8c42faa298d18f20177e4092502497ab
wenner <nelsondiasdemedeiros@gmail.com>
2020-07-25 18:53:27 -0300

    chore(config): configured server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,15 @@
+import { errors } from 'celebrate';
+import consola from 'consola';
 import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(errors());
+
+app.listen(process.env.APP_PORT, () => {
+  consola.success({message: `⚡ Server start with successfully on PORT ${process.env.APP_PORT} ⚡`, badge: true});
+})
