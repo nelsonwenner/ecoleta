69599279437cea0a1e969962642ebc8c0ae6995c
wenner <nelsondiasdemedeiros@gmail.com>
2020-07-25 19:05:22 -0300

    chore(upload): added config from path of uploads

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import { errors } from 'celebrate';
 import consola from 'consola';
 import express from 'express';
+import path from 'path';
 import cors from 'cors';
 import 'dotenv/config';
 
@@ -8,6 +9,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
+
 app.use(errors());
 
 app.listen(process.env.APP_PORT, () => {
