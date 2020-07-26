8f0e39fa6da5eb01887ceacbabe6f7724d757e37
wenner <nelsondiasdemedeiros@gmail.com>
2020-07-26 16:01:34 -0300

    chore(assets): added assets

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './styles.css';
 
+import Home from './pages/Home';
+
 const App = () => {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Home />
   );
 }
 
