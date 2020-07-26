189a3ef7bae8762a683479703dda92001ab2a22f
wenner <nelsondiasdemedeiros@gmail.com>
2020-07-26 17:31:23 -0300

    feat(header): create header

--- frontend/src/pages/CreatePoint/index.jsx
+++ frontend/src/pages/CreatePoint/index.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { FiArrowLeft } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 import './styles.css';
+
+import Logo from '../../assets/logo.svg';
 
 const CreatePoint = () => {
   return (
-    <h1>Hello World</h1>
+    <div className="page-create-point">
+      <header>
+        <img src={ Logo } alt="Ecollection" />
+
+        <Link to="/"> 
+          <FiArrowLeft /> Back to home
+        </Link>
+      </header>
+    </div>
   )
 }
 
