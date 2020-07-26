50263f17bad597e76835946d0cd4363d61298ed1
wenner <nelsondiasdemedeiros@gmail.com>
2020-07-25 22:50:08 -0300

    feat(pointController): create show from pointcontroller

--- backend/src/controllers/points.controller.ts
+++ backend/src/controllers/points.controller.ts
@@ -5,7 +5,7 @@ class PointsController {
 
   async index(req: Request, res: Response) {
     const { city, uf, items } = req.query;
-    
+
     const parsedItems = String(items)
       .split(',')
       .map(item => Number(item.trim()));
@@ -26,6 +26,28 @@ class PointsController {
     });
     
     return res.json(serializedPoints);
+  }
+
+  async show(req: Request, res: Response) {
+    const { id } = req.params;
+
+    const point = await knex('points').where('id', id).first();
+
+    if (!point) {
+      return res.status(400).json({error: 'Point does not found'});
+    }
+
+    const serializedPoint = {
+      ...point,
+      image_url: `${process.env.APP_URL}/uploads/${point.image}`
+    }
+
+    const items = await knex('items')
+    .join('point_items', 'items.id', '=', 'point_items.item_id')
+    .where('point_items.point_id', id)
+    .select('items.title');
+
+    return res.json({point: serializedPoint, items});
   }
 
   async store(req: Request, res: Response) {
--- backend/src/routes/points.router.ts
+++ backend/src/routes/points.router.ts
@@ -4,6 +4,6 @@ import router from './config.router';
 export default [
 
   router.post('/points', pointController.store),
-  router.get('/points', pointController.index)
-
+  router.get('/points', pointController.index),
+  router.get('/points/:id', pointController.show)
 ]
