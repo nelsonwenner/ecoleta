8fc30aec4c8fbb3ae05f2d652d8ca6c31e70897e
wenner <nelsondiasdemedeiros@gmail.com>
2020-07-27 16:37:15 -0300

    feat(Dropzone): create Dropzone

---
+++ frontend/src/components/Dropzone/index.jsx
@@ -0,0 +1,38 @@
+import React,{ useState, useCallback } from 'react';
+import { useDropzone } from 'react-dropzone';
+import { FiUpload } from 'react-icons/fi';
+import './styles.css';
+
+const Dropzone = ({ onFileUpload }) => {
+  const [selectedFileUrl, setSelectedFileUrl] = useState('');
+
+  const onDrop = useCallback((acceptedFile) => {
+    const file = acceptedFile[0];
+    const fileUrl = URL.createObjectURL(file);
+    setSelectedFileUrl(fileUrl);
+    onFileUpload(file);
+  });
+
+  const { getRootProps, getInputProps } = useDropzone({onDrop, accept: 'image/*'});
+
+  return (
+    <div className="dropzone" { ...getRootProps() }>
+      <input { ...getInputProps() } accept='image/*' />
+
+      {selectedFileUrl ? (
+
+        <img src={ selectedFileUrl } alt='thumbnail point' />
+
+      ) : (
+
+        <p>
+          <FiUpload />
+          Image
+        </p>
+
+      )}
+    </div>
+  )
+}
+
+export default Dropzone;
--- frontend/src/pages/CreatePoint/index.jsx
+++ frontend/src/pages/CreatePoint/index.jsx
@@ -93,6 +93,30 @@ const CreatePoint = () => {
     }
   }
   
+  const handlerSubmit = async (event) => {
+    event.preventDefault();
+
+    const { name, email, whatsapp } = formData;
+    const [latitude, longitude] = selectedPosition;
+    const uf = selectedUf;
+    const city = selectedCity;
+    const items = selectedItems;
+    
+    const data = new FormData();
+    
+    data.append('name', name);
+    data.append('email', email);
+    data.append('whatsapp', whatsapp);
+    data.append('uf', uf);
+    data.append('city', city);
+    data.append('latitude', String(latitude));
+    data.append('longitude', String(longitude));
+    data.append('items', items.join(','));
+
+    await api.post('/points', data);
+
+  } 
+
   return (
     <div className="page-create-point">
       <header>
@@ -103,7 +127,7 @@ const CreatePoint = () => {
         </Link>
       </header>
 
-      <form>
+      <form onSubmit={ handlerSubmit }>
         <h1>Registration of 
           <br/> the collection point
         </h1>
